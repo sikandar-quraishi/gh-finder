ec91bd10753bf56f36b4efd33da5c493cdea4fbb
zuaam <nriittech@gmail.com>
2020-01-14 07:23:28 -0800

    Updates

---
+++ precache-manifest.786f740836a5ffa6ccf31e2c55011ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42547aecc2b0e3f06f75867a0fc599b2",
+    "url": "/gh-finder/index.html"
+  },
+  {
+    "revision": "d97fd153838ce8a9b898",
+    "url": "/gh-finder/static/css/main.3520b3a2.chunk.css"
+  },
+  {
+    "revision": "c4c9329568caa652a79d",
+    "url": "/gh-finder/static/js/2.65039fc1.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/gh-finder/static/js/2.65039fc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d97fd153838ce8a9b898",
+    "url": "/gh-finder/static/js/main.c3e45013.chunk.js"
+  },
+  {
+    "revision": "f43fa8f957e7bf94633f",
+    "url": "/gh-finder/static/js/runtime-main.042a4902.js"
+  }
+]);
